0c3b3e27870466d5c9415d2e5e6c8938959a5258
Joosep <joosep@reach-u.com>
2022-12-04 12:08:36 +0200

    simplified second part conditions.

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -7,7 +7,7 @@ def has_full_overlap(f1, l1, f2, l2):
 
 def has_overlap(f1, l1, f2, l2):
     f1, l1, f2, l2 = int(f1), int(l1), int(f2), int(l2)
-    return f2 <= f1 <= l2 or f2 <= l1 <= l2 or f1 <= f2 <= l1 or f1 <= l2 <= l1
+    return f1 <= l2 and f2 <= l1
 
 
 def calc_part1(input_file):
