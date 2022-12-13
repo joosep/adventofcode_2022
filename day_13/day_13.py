7d998a2a953679a7835da55e4f05ee005a108f71
Joosep <joosep@reach-u.com>
2022-12-13 19:34:17 +0200

    day_13 part 2

--- day_13/day_13.py
+++ day_13/day_13.py
@@ -1,6 +1,7 @@
 # https://adventofcode.com/2022/day/13
 
 import json
+from functools import cmp_to_key
 
 
 def read_input(data):
@@ -10,49 +11,48 @@ def read_input(data):
     return converted_packages
 
 
-def is_in_order(left, right):
+def compare_lists(left, right):
     if isinstance(left, int) and isinstance(right, int):
-        if left < right:
-            return True
-        if right < left:
-            return False
-        return None
+        return left - right
     if isinstance(left, list) and isinstance(right, list):
         for i in range(0, min(len(left), len(right))):
-            order = is_in_order(left[i], right[i])
-            if order is not None:
+            order = compare_lists(left[i], right[i])
+            if order != 0:
                 return order
-        if len(left) < len(right):
-            return True
-        if len(right) < len(left):
-            return False
-        return None
+        return len(left) - len(right)
     if isinstance(left, int):
-        return is_in_order([left], right)
+        return compare_lists([left], right)
     if isinstance(right, int):
-        return is_in_order(left, [right])
+        return compare_lists(left, [right])
 
 
 def get_part1(data):
     packages = read_input(data)
     in_order_sum = 0
     for i in range(0, len(packages)):
-        in_order_sum += i + 1 if is_in_order(*packages[i]) else 0
+        in_order_sum += i + 1 if compare_lists(*packages[i]) < 0 else 0
     return in_order_sum
 
 
 def get_part2(data):
-    pass
+    packages = [list for t in read_input(data) for list in t]
+    packages.append([[2]])
+    packages.append([[6]])
+    packages = sorted(packages, key=cmp_to_key(compare_lists))
+    decode_key = 1
+    for i in range(0, len(packages)):
+        decode_key *= i + 1 if packages[i] == [[2]] or packages[i] == [[6]] else 1
+    return decode_key
 
 
 result = get_part1("test_data")
 assert result == 13, f"got: {result}"
 
 result = get_part1("input_data")
-assert result == None, f"got: {result}"
+assert result == 5185, f"got: {result}"
 
 result = get_part2("test_data")
-assert result == None, f"got: \n{result}"
+assert result == 140, f"got: \n{result}"
 
 result = get_part2("input_data")
-assert result == None, f"got: \n{result}"
+assert result == 23751, f"got: \n{result}"
